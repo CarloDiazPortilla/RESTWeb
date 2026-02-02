76d8e656f7b74a2b07cadd170d375761738274e6
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-02-02 12:14:30 -0500

    feat: add environment variables configuration

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,27 +1,36 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { envs } from "../config/plugins/envs.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+interface ServerOptions {
+  port: number,
+  publicPath: string,
+}
 
 export class Server {
 
   private static app = express();
 
+  private static readonly PORT: number = envs.PORT;
+  private static readonly PUBLIC_PATH: string = envs.PUBLIC_PATH;
+
   static async run() {
 
     // Public resources
-    this.app.use(express.static("public"));
+    this.app.use(express.static(this.PUBLIC_PATH));
 
     this.app.get("/{*splat}", (req, res) => {
-      const indexPath = path.join(__dirname + "../../../public/index.html");
+      const indexPath = path.join(__dirname + `../../../${this.PUBLIC_PATH}/index.html`);
       res.sendFile(indexPath);
       return;
     })
 
-    this.app.listen(3000, () => {
-      console.log(`Server running on port 3000`);
+    this.app.listen(this.PORT, () => {
+      console.log(`Server running on port ${this.PORT}`);
     })
   }
 }
