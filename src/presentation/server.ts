9ec3bd56b74dd1b7cfa21442e2a99a792b9c052b
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-02-02 10:54:14 -0500

    feat: serving single page application

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class Server {
 
@@ -8,6 +13,12 @@ export class Server {
 
     // Public resources
     this.app.use(express.static("public"));
+
+    this.app.get("/{*splat}", (req, res) => {
+      const indexPath = path.join(__dirname + "../../../public/index.html");
+      res.sendFile(indexPath);
+      return;
+    })
 
     this.app.listen(3000, () => {
       console.log(`Server running on port 3000`);
